fc46b884f5255ecf06e9a2ec5e000bab9f328ba7
GUNJAN KOSTA <gunjan.kosta2023@ssipmt.com>
2025-09-17 18:50:30 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/", // <-- replace "portfolio" with your repo name
+  base: "/portfolio-site/", // <-- replace "portfolio" with your repo name
 });

a029ec57c6602678698f69edd580726cf7c6f66f
GUNJAN KOSTA <gunjan.kosta2023@ssipmt.com>
2025-09-17 18:53:07 +0530

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Replace `my-portfolio` with your repo name
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [react()],
-  base: "/portfolio-site/", // <-- replace "portfolio" with your repo name
 });
